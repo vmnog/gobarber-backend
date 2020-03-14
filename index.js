6f06096228ff4dbb3682c20ff245791c3696f105
Victor <vmnogueira.ti@gmail.com>
2020-03-14 12:46:58 -0300

    Basic get query + nodemon

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import express from 'express';
 
-console.log(express);
+const server = express();
+
+server.get('/', (req, res) => {
+  const { nome } = req.query;
+
+  return res.json({ message: `Oi ${nome}` })
+})
+
+server.listen(3000);
