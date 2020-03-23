b1e7efd117d60635ae7daf8841e1fee6d904c712
Victor <vmnogueira.ti@gmail.com>
2020-03-23 19:28:40 -0300

    Validating UserController  with Yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,25 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6)
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: 'Validation fails'
+      });
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
@@ -21,6 +39,28 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string(),
+      // Se usuario enviar oldPassword entao o campo password é obrigatorio na atualizacao de senha
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) => {
+          return oldPassword ? field.required() : field;
+        }),
+      confirmPassword: Yup.string().when('password', (password, field) => {
+        // confirm password tem que estar entre os valores de password
+        return password ? field.required().oneOf([Yup.ref('password')]) : field;
+      })
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: 'Validation fails'
+      });
+    }
+
     const { email, password, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
