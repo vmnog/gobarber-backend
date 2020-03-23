73a92421dc97aaebb24b3e03e1be437cce530087
Victor <vmnogueira.ti@gmail.com>
2020-03-23 20:32:42 -0300

    Create FileController and Sequelize Migration

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 import fileMiddleware from './app/middlewares/file';
@@ -16,8 +17,6 @@ routes.put('/users/:id', authMiddleware, UserController.update);
 routes.post('/sessions', SessionController.store);
 
 //Files
-routes.post('/files', authMiddleware, fileMiddleware, (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', authMiddleware, fileMiddleware, FileController.store);
 
 export default routes;
