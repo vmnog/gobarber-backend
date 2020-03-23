b52581fb17fad28d52ba15c8bc4f6dde6ccf7eec
Victor <vmnogueira.ti@gmail.com>
2020-03-22 22:43:49 -0300

    update user route

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -21,9 +21,42 @@ class UserController {
   }
 
   async update(req, res) {
-    const { userId } = req;
+    const { email, password, oldPassword } = req.body;
 
-    return res.json({ userId });
+    const user = await User.findByPk(req.userId);
+
+    // se usuario ta tentando alterar o email
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: {
+          email
+        }
+      });
+
+      if (userExists) {
+        return res.status(401).json({
+          error: 'E-mail already registered.'
+        });
+      }
+    }
+
+    // compara senha antiga com senha nova
+    // Só vai fazer a comparação caso o parametro oldpassword exista
+    // se oldpassword n existe entao o usuario n quer alterar a senha
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({
+        error: 'Password does not match.'
+      });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.status(200).json({
+      id,
+      name,
+      email,
+      provider
+    });
   }
 }
 
