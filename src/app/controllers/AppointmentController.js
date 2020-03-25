c9b20079344702fe9b81343c5c93da3941d0eb1e
Victor <vmnogueira.ti@gmail.com>
2020-03-25 11:21:03 -0300

    Create appointments with date, user logged and provider id

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,6 @@
 import Appointment from '../models/Appointment';
+import User from '../models/User';
+
 import * as Yup from 'yup';
 
 class AppointmentController {
@@ -14,9 +16,41 @@ class AppointmentController {
       });
     }
 
-    const { provider_id, data } = req.body;
+    console.log('1. validação ok\n\n\n');
 
-    return res.json();
+    const { provider_id, date } = req.body;
+
+    // Check if provider_id exists and if it is a provider
+    const isProvider = await User.findOne({
+      where: {
+        id: provider_id,
+        provider: true
+      }
+    });
+
+    if (!isProvider) {
+      return res.status(401).json({
+        error: 'You can only create appointent with providers'
+      });
+    }
+
+    console.log('2. provider ok\n\n\n');
+
+    console.log({
+      id: req.userId,
+      provider: provider_id,
+      date: date
+    });
+
+    const appointment = await Appointment.create({
+      user_id: req.userId, // userId é enviado pelo middleware de auth, é o id do usuario logado
+      provider_id,
+      date
+    });
+
+    console.log('3. appointment create  ok\n\n\n');
+
+    return res.json(appointment);
   }
 }
 
