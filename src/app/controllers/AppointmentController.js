e7f51876f13ee718623195e75c73040f39792b5e
Victor <vmnogueira.ti@gmail.com>
2020-03-25 11:08:59 -0300

    Validate appointment req.body request

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,7 +1,21 @@
 import Appointment from '../models/Appointment';
+import * as Yup from 'yup';
 
 class AppointmentController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: 'Appointment validation failed'
+      });
+    }
+
+    const { provider_id, data } = req.body;
+
     return res.json();
   }
 }
